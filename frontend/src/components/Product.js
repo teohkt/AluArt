de4f70a4b80afeaa4dbe3e82d889db14061abbb1
Kelvin Teoh <kelvin.ts.teoh@gmail.com>
2021-03-22 15:02:14 -0400

    added rating component which displays stars and # of ratings

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 
+import Rating from './Rating'
+
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
@@ -16,7 +18,10 @@ const Product = ({ product }) => {
         </a>
         <Card.Text as='div'>
           <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
+            <Rating
+              rating={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
           </div>
         </Card.Text>
 
---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Rating = ({ rating, text, color }) => {
+  return (
+    <div className='rating'>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            rating >= 1
+              ? 'fas fa-star'
+              : rating >= 0.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+        <i
+          style={{ color }}
+          className={
+            rating >= 2
+              ? 'fas fa-star'
+              : rating >= 1.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+        <i
+          style={{ color }}
+          className={
+            rating >= 3
+              ? 'fas fa-star'
+              : rating >= 2.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+        <i
+          style={{ color }}
+          className={
+            rating >= 4
+              ? 'fas fa-star'
+              : rating >= 3.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+        <i
+          style={{ color }}
+          className={
+            rating >= 5
+              ? 'fas fa-star'
+              : rating >= 4.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <p>{text && text}</p>
+    </div>
+  )
+}
+
+Rating.defaultProps = {
+  color: 'rgb(255,215,0)',
+}
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+}
+export default Rating
