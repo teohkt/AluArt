4ecaa47f45c3b05a9293223cae6fca6838c68c3b
Kelvin Teoh <kelvin.ts.teoh@gmail.com>
2021-03-23 09:08:04 -0400

    added react-router-dom and react-router-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
 import { Container, Row, Col } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main>
         <Container>
           <Row>
             <Col className='py-3'>
-              <HomeScreen />
+              <Route path='/' exact component={HomeScreen} />
+              <Route path='/product/:id' component={ProductScreen} />
             </Col>
           </Row>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { ShoppingCart, LogIn } from 'react-feather'
 
@@ -7,18 +8,25 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>CANALU</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>CANALU</Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
-                <ShoppingCart size={14} style={{ margin: '0 6px 3px 0' }} />
-                Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <LogIn size={16} style={{ margin: '0 6px 3px 0' }} />
-                Sign In
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <ShoppingCart size={14} style={{ margin: '0 6px 3px 0' }} />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <LogIn size={16} style={{ margin: '0 6px 3px 0' }} />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
