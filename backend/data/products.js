3008954f82a9ee3e24dfa33d12224346aa64fbe3
Kelvin Teoh <kelvin.ts.teoh@gmail.com>
2021-03-23 20:30:27 -0400

    started backend with products route

---
+++ backend/data/products.js
@@ -0,0 +1,95 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Blue Orange Can',
+    image: '/images/blue_orange.jpg',
+    description:
+      'Artistic can with misleading labels to really confuse guests and art connoisseur.',
+    brand: 'Goof',
+    category: 'Art',
+    price: 29.99,
+    countInStock: 3,
+    rating: 4.6,
+    numReviews: 38,
+  },
+  {
+    _id: '2',
+    name: 'Bart Art Soda',
+    image: '/images/bart.jpg',
+    description:
+      'Scate with the cool kids not only in this season, and forever with this can.',
+    brand: 'Riders',
+    category: 'Art',
+    price: 24.99,
+    countInStock: 7,
+    rating: 4.1,
+    numReviews: 32,
+  },
+  {
+    _id: '3',
+    name: 'Virus of the Decade',
+    image: '/images/virus.jpg',
+    description:
+      'Ever forget what its like to be quarantied for a long time? Remember the time with this can.',
+    brand: 'Memories',
+    category: 'Art',
+    price: 19.99,
+    countInStock: 69,
+    rating: 2.0,
+    numReviews: 49,
+  },
+  {
+    _id: '4',
+    name: 'Chopter Can',
+    image: '/images/plane.jpg',
+    description:
+      'Appreciate the slow moving aircrafts of ole with this slow moving soda.',
+    brand: 'SLO',
+    category: 'Art',
+    price: 9.99,
+    countInStock: 23,
+    rating: 4.3,
+    numReviews: 54,
+  },
+  {
+    _id: '5',
+    name: 'Aroma Therapy',
+    image: '/images/fish.jpg',
+    description:
+      'The times of candles are over. Step up your lonely game by bringing home the forever in your life.',
+    brand: 'Plenty of Fish',
+    category: 'Art',
+    price: 69.69,
+    countInStock: 34,
+    rating: 4.8,
+    numReviews: 87,
+  },
+  {
+    _id: '6',
+    name: 'Graphitti',
+    image: '/images/graphitti.jpg',
+    description:
+      'Need to fake it till you make it? Grab these authentic cans to show people what you are really made of.',
+    brand: 'Gorillza',
+    category: 'Art',
+    price: 42.0,
+    countInStock: 0,
+    rating: 4.2,
+    numReviews: 87,
+  },
+  {
+    _id: '7',
+    name: 'Can Holder',
+    image: '/images/hand.jpg',
+    description:
+      'Dont ruin your art with oily fingerprints. Use this one instead',
+    brand: 'Human Replica',
+    category: 'Art',
+    price: 19.99,
+    countInStock: 19,
+    rating: 4.7,
+    numReviews: 84,
+  },
+]
+
+module.exports = products
