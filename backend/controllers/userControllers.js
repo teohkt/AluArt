a472798b74e8f6d924f1e49f94316216ed229d37
teohkt <kelvin.ts.teoh@gmail.com>
2021-03-27 18:33:22 -0400

    created user registration in backend

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -21,6 +21,38 @@ const authUser = asyncHandler (async(req, res) => {
         res.status(401)
         throw new Error('Invalid email or password')
     }
+})
+
+// @ desc   Register a new user
+// @ route  post /api/users
+// @ access Public
+const registerUser = asyncHandler (async(req, res) => {
+    const { name, email, password } = req.body
+    const userExists = await User.findOne({ email })
+
+    if (userExists){
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid User Data')
+    }
 
 })
 
@@ -44,5 +76,6 @@ const getUserProfile = async(req, res) => {
 
 export {
     authUser,
-    getUserProfile
+    getUserProfile,
+    registerUser
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,10 +4,13 @@ import { protect } from '../middleware/authMiddleware.js'
 
 import {
     authUser,
+    registerUser,
     getUserProfile
 } from '../controllers/userControllers.js'
 
+router.route('/').post(registerUser)
 router.route('/login').post(authUser)
 router.route('/profile').get(protect, getUserProfile)
+
 
 export default router
