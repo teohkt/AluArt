86914cd5ba21f18fec9c717b0df0f2d8ebfef1b5
teohkt <kelvin.ts.teoh@gmail.com>
2021-03-27 15:42:10 -0400

    user login/authorization

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,15 +1,12 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @ desc   Authenticate email & password, get token
 // @ route  post /api/users/login
 // @ access Public
 const authUser = asyncHandler (async(req, res) => {
     const { email, password } = req.body
-    // res.send({
-    //     email,
-    //     password
-    // })
 
     const user = await User.findOne({ email })
     if (user && (await user.matchPassword(password))) {
@@ -18,7 +15,7 @@ const authUser = asyncHandler (async(req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
@@ -27,6 +24,25 @@ const authUser = asyncHandler (async(req, res) => {
 
 })
 
+// @ desc   Get user profile
+// @ route  GET /api/users/profile
+// @ access Private
+const getUserProfile = async(req, res) => {
+    const user = req.user
+    if(user){
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+}
+
 export {
-    authUser
+    authUser,
+    getUserProfile
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
 const router = express.Router()
+import { protect } from '../middleware/authMiddleware.js'
+
 import {
-    authUser
+    authUser,
+    getUserProfile
 } from '../controllers/userControllers.js'
 
 router.route('/login').post(authUser)
-// router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
