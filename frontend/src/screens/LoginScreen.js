a97fad9368e5dbeef5a92027b35d1b4b70b91925
teohkt <kelvin.ts.teoh@gmail.com>
2021-03-29 16:09:00 -0400

    added user login frontend

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import FormContainer from '../components/FormContainer'
+
+import { Login } from '../actions/userActions'
+
+const LoginScreen = (props) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { loading, error, userInfo } = userLogin
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(Login(email, password))
+  }
+
+  const redirect = props.location.search
+    ? props.location.search.split('=')[1]
+    : '/'
+
+  useEffect(() => {
+    if (userInfo) {
+      props.history.push(redirect)
+    }
+  }, [props.history, userInfo, redirect])
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className='py-3'>
+        <Col>
+          New Customer?{' '}
+          <Link to={redirect ? '/register?redirect=${redirect}' : '/register'}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  )
+}
+
+export default LoginScreen
