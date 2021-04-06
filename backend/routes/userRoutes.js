35cd14e9da863688c8c9780534391250e4868348
teohkt <kelvin.ts.teoh@gmail.com>
2021-04-06 18:32:26 -0400

    created isAdmin verification middleware and route to pull details on users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express'
 const router = express.Router()
-import { protect } from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 import {
   authUser,
   registerUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
 } from '../controllers/userControllers.js'
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.route('/login').post(authUser)
 router
   .route('/profile')
