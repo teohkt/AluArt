974cf49798145087a871325e4e7e78739b1f4c1b
teohkt <kelvin.ts.teoh@gmail.com>
2021-04-15 20:02:24 -0400

    created heroku script for build

--- backend/server.js
+++ backend/server.js
@@ -23,30 +23,29 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json()) //Alows us to accept json data from form data
 
-app.get('/', (req, res) => {
-  res.send('API is running')
-})
-
 app.use('/api/product', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
-app.get('/api/config/paypal', (req, res) =>
-  res.send(process.env.PAYPAL_CLIENT_ID)
-)
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads'))) // makes uploads folder accessible by the frontend
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
-)
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
